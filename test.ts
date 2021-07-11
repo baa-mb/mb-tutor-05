f144f3963212853c854d5a3a0ed0cc756d6cd86d
BAA2 <alois.bachinger@ph-linz.at>
2021-07-11 15:44:42 +0200

    ssss

--- test.ts
+++ test.ts
@@ -1,1 +1,1 @@
-// Hier kann man Tests durchführen; diese Datei wird nicht kompiliert, wenn dieses Paket als Erweiterung verwendet wird.
+// xxxxHier kann man Tests durchführen; diese Datei wird nicht kompiliert, wenn dieses Paket als Erweiterung verwendet wird.
